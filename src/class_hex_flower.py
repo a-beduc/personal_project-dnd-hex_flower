9647a9e55fb6c0cb2d93d6b26e9cf68b7e8e4558
Alexandre Beduc <alexandre.beduc@gmail.com>
2024-07-07 17:06:03 +0200

    getting rid of useless variable z that was (y - x)

--- src/class_hex_flower.py
+++ src/class_hex_flower.py
@@ -3,32 +3,31 @@ import random
 
 
 class Hex:
-    def __init__(self, x, y, z, title, description, effect=None):
+    def __init__(self, x, y, title, description, effect=None):
         self.x = x
         self.y = y
-        self.z = z
         self.title = title
         self.description = description
         self.effect = effect
 
     def __repr__(self):
-        return f"Hex({self.x}, {self.y}, {self.z}, self.{self.title}, {self.description}, {self.effect})"
+        return f"Hex({self.x}, {self.y}, self.{self.title}, {self.description}, {self.effect})"
 
 
 class HexGrid:
-    DIRECTIONS = {"N": (1, 0, -1), "NE": (1, 1, 0), "SE": (0, 1, 1),
-                  "S": (-1, 0, 1), "SW": (-1, -1, 0), "NW": (0, -1, -1)}
+    DIRECTIONS = {"N": (1, 0), "NE": (1, 1), "SE": (0, 1),
+                  "S": (-1, 0), "SW": (-1, -1), "NW": (0, -1)}
 
-    IMPOSSIBLE_MOVES = {((2, 0, -2), "N"), ((1, -1, -2), "N"), ((2, 1, -1), "N")}
+    IMPOSSIBLE_MOVES = {((2, 0), "N"), ((1, -1), "N"), ((2, 1), "N")}
 
     LOOPS = {
-        "WEST": {((0, -2, -2), "SW"): (-1, -2, -1), ((-2, -2, 0), "NW"): (-1, -2, -1)},
-        "EAST": {((2, 2, 0), "SE"): (1, 2, 1), ((0, 2, 2), "NE"): (1, 2, 1)},
-        "SOUTH_WEST": {((-2, 0, 2), "SW"): (-2, -1, 1)},
-        "SOUTH_EAST": {((-2, 0, 2), "SE"): (-1, 1, 2)}
+        "WEST": {((0, -2), "SW"): (-1, -2), ((-2, -2), "NW"): (-1, -2)},
+        "EAST": {((2, 2), "SE"): (1, 2), ((0, 2), "NE"): (1, 2)},
+        "SOUTH_WEST": {((-2, 0), "SW"): (-2, -1)},
+        "SOUTH_EAST": {((-2, 0), "SE"): (-1, 1)}
     }
 
-    def __init__(self, init_file=None, current_position=(1, 0, -1)):
+    def __init__(self, init_file=None, current_position=(1, 0)):
         self.min_coord = -2
         self.max_coord = 2
         self.grid = self.init_grid(init_file)
@@ -41,11 +40,11 @@ class HexGrid:
             with open(init_file, "r") as file:
                 data = json.load(file)
                 for key, value in data.items():
-                    x, y, z = eval(key)
+                    x, y = eval(key)
                     title = value["title"]
                     description = value["description"]
                     effect = value["effect"]
-                    grid[(x, y, z)] = Hex(x, y, z, title, description, effect)
+                    grid[(x, y)] = Hex(x, y, title, description, effect)
         else:
             print("error, no file given")
         return grid
@@ -65,23 +64,22 @@ class HexGrid:
         new_coordinates = [
             coordinates[0] + direction[0],
             coordinates[1] + direction[1],
-            coordinates[2] + direction[2]
         ]
-        x, y, z = new_coordinates
+        x, y = new_coordinates
 
         if (direction_name in ("N", "S") and
                 (x < self.min_coord or x > self.max_coord or
-                 z < self.min_coord or z > self.max_coord)):
-            new_coordinates = [coordinates[2], coordinates[1], coordinates[0]]
+                 (y-x) < self.min_coord or (y-x) > self.max_coord)):
+            new_coordinates = [coordinates[1]-coordinates[0], coordinates[1]]
 
         if (direction_name in ("NW", "SW", "NE", "SE") and
                 (x < self.min_coord or x > self.max_coord or
                  y < self.min_coord or y > self.max_coord or
-                 z < self.min_coord or z > self.max_coord)):
+                 (y-x) < self.min_coord or (y-x) > self.max_coord)):
             if direction_name in ("NW", "SE"):
-                new_coordinates = [coordinates[0], -coordinates[2], -coordinates[1]]
+                new_coordinates = [coordinates[0], -(coordinates[1]-coordinates[0])]
             else:
-                new_coordinates = [-coordinates[1], -coordinates[0], coordinates[2]]
+                new_coordinates = [-coordinates[1], -coordinates[0]]
         return tuple(new_coordinates)
 
     def move_current_position(self, direction_name):
@@ -113,55 +111,55 @@ def random_move():
 
 def main():
     print("*****-- LOOP EAST 1 --******")
-    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(2, 2, 0))
+    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(2, 2))
     print(f"Current position : {new_hex_grid.current_position}")
     new_position = new_hex_grid.move_current_position("SE")
     print(f"Current position : {new_position}\n")
 
     print("*****-- LOOP EAST 2 --******")
-    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(0, 2, 2))
+    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(0, 2))
     print(f"Current position : {new_hex_grid.current_position}")
     new_position = new_hex_grid.move_current_position("NE")
     print(f"Current position : {new_position}\n")
 
     print("*****-- LOOP WEST 1 --******")
-    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(0, -2, -2))
+    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(0, -2))
     print(f"Current position : {new_hex_grid.current_position}")
     new_position = new_hex_grid.move_current_position("SW")
     print(f"Current position : {new_position}\n")
 
     print("*****-- LOOP WEST 2 --******")
-    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(-2, -2, 0))
+    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(-2, -2))
     print(f"Current position : {new_hex_grid.current_position}")
     new_position = new_hex_grid.move_current_position("NW")
     print(f"Current position : {new_position}\n")
 
     print("*****-- LOOP SOUTH EAST --******")
-    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(-2, 0, 2))
+    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(-2, 0))
     print(f"Current position : {new_hex_grid.current_position}")
     new_position = new_hex_grid.move_current_position("SE")
     print(f"Current position : {new_position}\n")
 
     print("*****-- LOOP SOUTH WEST --******")
-    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(-2, 0, 2))
+    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(-2, 0))
     print(f"Current position : {new_hex_grid.current_position}")
     new_position = new_hex_grid.move_current_position("SW")
     print(f"Current position : {new_position}\n")
 
     print("*****-- STOP NORTH WEST --******")
-    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(1, -1, -2))
+    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(1, -1))
     print(f"Current position : {new_hex_grid.current_position}")
     new_position = new_hex_grid.move_current_position("N")
     print(f"Current position : {new_position}\n")
 
     print("*****-- STOP NORTH EAST --******")
-    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(2, 1, -1))
+    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(2, 1))
     print(f"Current position : {new_hex_grid.current_position}")
     new_position = new_hex_grid.move_current_position("N")
     print(f"Current position : {new_position}\n")
 
     print("*****-- STOP NORTH NORTH --******")
-    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(2, 0, -2))
+    new_hex_grid = HexGrid(init_file="weather_flower.json", current_position=(2, 0))
     print(f"Current position : {new_hex_grid.current_position}")
     new_position = new_hex_grid.move_current_position("N")
     print(f"Current position : {new_position}\n")
